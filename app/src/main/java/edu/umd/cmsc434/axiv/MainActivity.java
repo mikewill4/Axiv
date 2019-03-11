aa26aef981555862cfaa05028075793e26141966
Michael Williams <mjwilli8@umd.edu>
2019-03-10 21:04:35 -0400

    Created bottom navigation bar with fragments for each screen

--- app/src/main/java/edu/umd/cmsc434/axiv/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/MainActivity.java
@@ -1,13 +1,70 @@
 package edu.umd.cmsc434.axiv;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mainNavigation;
+    private FrameLayout mainFrame;
+
+    private MyHealthFragment myHealthFragment;
+    private MetricsFragment metricsFragment;
+    private CompeteFragment competeFragment;
+    private RewardsFragment rewardsFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mainNavigation = (BottomNavigationView) findViewById(R.id.main_navigation);
+
+        myHealthFragment = new MyHealthFragment();
+        metricsFragment = new MetricsFragment();
+        competeFragment = new CompeteFragment();
+        rewardsFragment = new RewardsFragment();
+        settingsFragment = new SettingsFragment();
+
+        setFragment(myHealthFragment);
+
+        mainNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_my_health:
+                        setFragment(myHealthFragment);
+                        return true;
+                    case R.id.navigation_metrics:
+                        setFragment(metricsFragment);
+                        return true;
+                    case R.id.navigation_compete:
+                        setFragment(competeFragment);
+                        return true;
+                    case R.id.navigation_rewards:
+                        setFragment(rewardsFragment);
+                        return true;
+                    case R.id.navigation_settings:
+                        setFragment(settingsFragment);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
