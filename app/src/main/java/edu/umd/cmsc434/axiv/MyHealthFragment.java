344d824872227631cd962ac67ad6fa2e90cb4e90
Michael Williams <mjwilli8@umd.edu>
2019-03-31 03:55:21 -0400

    Removed unused import

--- app/src/main/java/edu/umd/cmsc434/axiv/MyHealthFragment.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/MyHealthFragment.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
