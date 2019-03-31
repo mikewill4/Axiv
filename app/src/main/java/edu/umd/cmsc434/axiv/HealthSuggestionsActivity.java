fc6a5cb224115c80d7baea7269a39ac5f0acec1f
Michael Williams <mjwilli8@umd.edu>
2019-03-31 02:58:12 -0400

    Created activites for daily targets, health suggestions, and track progress

---
+++ app/src/main/java/edu/umd/cmsc434/axiv/HealthSuggestionsActivity.java
@@ -0,0 +1,13 @@
+package edu.umd.cmsc434.axiv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthSuggestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_suggestions);
+    }
+}
