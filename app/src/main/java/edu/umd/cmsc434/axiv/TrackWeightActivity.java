44bde858daa0e3cf746abd4b146e4f4a874edd7d
DylanZenoFox <dylfox21@gmail.com>
2019-04-02 21:09:02 -0400

    Track Weight Screen Layout

--- app/src/main/java/edu/umd/cmsc434/axiv/TrackWeightActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/TrackWeightActivity.java
@@ -1,11 +1,14 @@
 package edu.umd.cmsc434.axiv;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 
 public class TrackWeightActivity extends AppCompatActivity {
 
@@ -13,5 +16,19 @@ public class TrackWeightActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_weight);
+
+        Spinner unitSpinner = (Spinner) findViewById(R.id.input_unit_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.weight_units ,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinner.setAdapter(adapter);
+
+        
     }
+
+
+
+
+
+
 }
