569bda50a29fd067c60a60e010343c381d75ef3f
Michael Williams <mjwilli8@umd.edu>
2019-04-09 23:21:12 -0400

    Added listview with sample health suggestions

--- app/src/main/java/edu/umd/cmsc434/axiv/HealthSuggestionsActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/HealthSuggestionsActivity.java
@@ -2,12 +2,34 @@ package edu.umd.cmsc434.axiv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HealthSuggestionsActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_suggestions);
+
+        listView = (ListView) findViewById(R.id.health_suggestions_list);
+
+        List<String> healthSuggestions = new ArrayList<>();
+        healthSuggestions.add("Walk 5 miles every week.");
+        healthSuggestions.add("Read for 30 min before going to bed.");
+        healthSuggestions.add("Eat a salad for lunch.");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                healthSuggestions
+        );
+
+        listView.setAdapter(arrayAdapter);
     }
 }
