3489b4a9ac5ebe6a8ffa95d31ede9a90a7721c8c
Fritz Wiltman <fgwilt1@gmail.com>
2019-04-11 20:19:08 -0400

    Added registration/log in page to beginning, log in has most functionality, registration is limited at the moment.

--- app/src/main/java/edu/umd/cmsc434/axiv/LoginRegisterActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/LoginRegisterActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 public class LoginRegisterActivity extends AppCompatActivity {
 
