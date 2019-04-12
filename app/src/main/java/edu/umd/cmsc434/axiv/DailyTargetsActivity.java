c92baff9a5c805912d44ea74fd2278f014c8b296
Michael Williams <mjwilli8@umd.edu>
2019-04-12 01:17:36 -0400

    track progress chart updates

--- app/src/main/java/edu/umd/cmsc434/axiv/DailyTargetsActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/DailyTargetsActivity.java
@@ -1,13 +1,55 @@
 package edu.umd.cmsc434.axiv;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DailyTargetsActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_targets);
+
+        listView = (ListView) findViewById(R.id.daily_targets_list);
+
+        final List<String> healthSuggestions = new ArrayList<>();
+        healthSuggestions.add("Walk 5 miles every week.");
+        healthSuggestions.add("Read for 30 min before going to bed.");
+        healthSuggestions.add("Eat a salad for lunch.");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                healthSuggestions
+        );
+
+        listView.setAdapter(arrayAdapter);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                builder.setTitle(healthSuggestions.get(position));
+                builder.setMessage(R.string.more_information);
+                builder.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Dialog will be dismissed when clicked
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 }
--- app/src/main/java/edu/umd/cmsc434/axiv/TrackProgressActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/TrackProgressActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.view.Display;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
@@ -39,10 +44,33 @@ public class TrackProgressActivity extends AppCompatActivity  {
         chart.setMinimumHeight(size.x);
         chart.setMinimumWidth(size.x);
 
+        LimitLine upperHealthyLimit = new LimitLine(60f, "Healthy");
+        upperHealthyLimit.setLineWidth(4f);
+        upperHealthyLimit.enableDashedLine(10f, 10f, 0f);
+        upperHealthyLimit.setLabelPosition(LimitLine.LimitLabelPosition.LEFT_TOP);
+        upperHealthyLimit.setTextSize(16f);
+        LimitLine lowerHealthyLimit = new LimitLine(40f, "Healthy");
+        lowerHealthyLimit.setLineWidth(4f);
+        lowerHealthyLimit.enableDashedLine(10f, 10f, 0f);
+        lowerHealthyLimit.setLabelPosition(LimitLine.LimitLabelPosition.LEFT_BOTTOM);
+        lowerHealthyLimit.setTextSize(16f);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(upperHealthyLimit);
+        leftAxis.addLimitLine(lowerHealthyLimit);
+
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        //leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        chart.getAxisRight().setEnabled(false);
+
         ArrayList<Entry> yValues = new ArrayList<>();
 
-        for (int i = 0; i <= 100; i += 10) {
-            yValues.add(new Entry(0, (float)(i * Math.random())));
+        for (int i = 0; i <= 50; i += 10) {
+            yValues.add(new Entry(i / 10, (float)(100 * Math.random())));
         }
 
         LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
@@ -58,5 +86,25 @@ public class TrackProgressActivity extends AppCompatActivity  {
         LineData data = new LineData(dataSets);
 
         chart.setData(data);
+
+        String[] values = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun"};
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new XAxisValueFormatter(values));
+        //xAxis.setGranularity(1);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+    }
+
+    public class XAxisValueFormatter implements IAxisValueFormatter {
+        private String[] values;
+        public XAxisValueFormatter(String[] values) {
+            this.values = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return values[(int)value];
+        }
     }
 }
