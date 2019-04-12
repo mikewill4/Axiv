1e136ef474affa7a9066831f1d348abcac500368
Michael Williams <mjwilli8@umd.edu>
2019-04-12 00:34:50 -0400

    added a back button to register page

--- app/src/main/java/edu/umd/cmsc434/axiv/FirstRegisterFragment.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/FirstRegisterFragment.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FirstRegisterFragment extends Fragment {
+
+    private LoginFragment loginFragment;
 
     public FirstRegisterFragment() {
         // Required empty public constructor
@@ -22,6 +26,20 @@ public class FirstRegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_first_register, container, false);
+
+        TextView haveAccount = (TextView) rootView.findViewById(R.id.accountText);
+        haveAccount.setText(Html.fromHtml("<font color=#000000>Already have an account?</font> <font color=#fda500> Sign in.</font>"));
+
+        loginFragment = new LoginFragment();
+
+        haveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((LoginActivity)getActivity()).setFragment(loginFragment);
+            }
+        });
+
+        return rootView;
     }
 }
