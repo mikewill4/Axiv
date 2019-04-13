c1c2d78ba7be7959cb60a0cb9efa6bd43ebf3e63
Michael Williams <mjwilli8@umd.edu>
2019-04-12 21:44:42 -0400

    added swiping to main screens

--- app/src/main/java/edu/umd/cmsc434/axiv/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/MainActivity.java
@@ -3,7 +3,12 @@ package edu.umd.cmsc434.axiv;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,6 +16,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private int counter = 5;
 
+    // View pager
+    private static final int NUM_PAGES = 5;
+    private ViewPager pager;
+    private ViewPagerAdapter pagerAdapter;
+    private MenuItem prevMenuItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,43 +57,115 @@ public class MainActivity extends AppCompatActivity {
         rewardsFragment = new RewardsFragment();
         settingsFragment = new SettingsFragment();
 
-        setFragment(myHealthFragment);
+        //setFragment(myHealthFragment);
         getSupportActionBar().setTitle(R.string.my_health_title);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         mainNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_my_health:
-                        setFragment(myHealthFragment);
+                        //setFragment(myHealthFragment);
+                        pager.setCurrentItem(0);
                         getSupportActionBar().setTitle(R.string.my_health_title);
-                        return true;
+                        break;
                     case R.id.navigation_metrics:
-                        setFragment(metricsFragment);
+                        //setFragment(metricsFragment);
+                        pager.setCurrentItem(1);
                         getSupportActionBar().setTitle(R.string.track_metrics);
-                        return true;
+                        break;
                     case R.id.navigation_compete:
-                        setFragment(competeFragment);
+                        //setFragment(competeFragment);
+                        pager.setCurrentItem(2);
                         getSupportActionBar().setTitle(R.string.compete_title);
-                        return true;
+                        break;
                     case R.id.navigation_rewards:
-                        setFragment(rewardsFragment);
+                        //setFragment(rewardsFragment);
+                        pager.setCurrentItem(3);
                         getSupportActionBar().setTitle(R.string.rewards_title);
-                        return true;
+                        break;
                     case R.id.navigation_settings:
-                        setFragment(settingsFragment);
+                        //setFragment(settingsFragment);
+                        pager.setCurrentItem(4);
                         getSupportActionBar().setTitle(R.string.settings_title);
-                        return true;
+                        break;
                     default:
-                        return true;
+                        break;
                 }
+                return false;
             }
         });
+
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    mainNavigation.getMenu().getItem(position).setChecked(false);
+                }
+                mainNavigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = mainNavigation.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        pagerAdapter.addFragment(myHealthFragment);
+        pagerAdapter.addFragment(metricsFragment);
+        pagerAdapter.addFragment(competeFragment);
+        pagerAdapter.addFragment(rewardsFragment);
+        pagerAdapter.addFragment(settingsFragment);
+        pager.setAdapter(pagerAdapter);
     }
 
-    private void setFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
+//    private void setFragment(Fragment fragment) {
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+//        fragmentTransaction.replace(R.id.main_frame, fragment);
+//        fragmentTransaction.commit();
+//        manager.executePendingTransactions();
+//    }
+
+    @Override
+    public void onBackPressed() {
+        if (pager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            pager.setCurrentItem(pager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
     }
 }
