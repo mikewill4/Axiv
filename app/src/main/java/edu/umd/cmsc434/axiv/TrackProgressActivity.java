8db135499fd8331b9151b5d792182d9a955b1b25
Michael Williams <mjwilli8@umd.edu>
2019-04-18 12:56:01 -0400

    added new data for all metrics to line graph in track progress

--- app/src/main/java/edu/umd/cmsc434/axiv/TrackProgressActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/TrackProgressActivity.java
@@ -44,12 +44,12 @@ public class TrackProgressActivity extends AppCompatActivity  {
         chart.setMinimumHeight(size.x);
         chart.setMinimumWidth(size.x);
 
-        LimitLine upperHealthyLimit = new LimitLine(60f, "Healthy");
+        LimitLine upperHealthyLimit = new LimitLine(60f, "Healthy upper bound");
         upperHealthyLimit.setLineWidth(4f);
         upperHealthyLimit.enableDashedLine(10f, 10f, 0f);
         upperHealthyLimit.setLabelPosition(LimitLine.LimitLabelPosition.LEFT_TOP);
         upperHealthyLimit.setTextSize(16f);
-        LimitLine lowerHealthyLimit = new LimitLine(40f, "Healthy");
+        LimitLine lowerHealthyLimit = new LimitLine(40f, "Healthy lower bound");
         lowerHealthyLimit.setLineWidth(4f);
         lowerHealthyLimit.enableDashedLine(10f, 10f, 0f);
         lowerHealthyLimit.setLabelPosition(LimitLine.LimitLabelPosition.LEFT_BOTTOM);
@@ -67,24 +67,129 @@ public class TrackProgressActivity extends AppCompatActivity  {
 
         chart.getAxisRight().setEnabled(false);
 
-        ArrayList<Entry> yValues = new ArrayList<>();
+        // Heart rate data
+        ArrayList<Entry> heartRateValues = new ArrayList<>();
 
         for (int i = 0; i <= 50; i += 10) {
-            yValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+            heartRateValues.add(new Entry(i / 10, (float)(100 * Math.random())));
         }
 
-        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+        LineDataSet heartRateSet = new LineDataSet(heartRateValues, "Heart rate");
 
-        set1.setFillAlpha(110);
-        set1.setColor(Color.RED);
-        set1.setLineWidth(3f);
-        set1.setValueTextSize(16);
+        heartRateSet.setFillAlpha(110);
+        heartRateSet.setColor(Color.RED);
+        heartRateSet.setLineWidth(3f);
+        heartRateSet.setValueTextSize(16);
 
+        // Hydration
+        ArrayList<Entry> hydrationValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            hydrationValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet hydrationSet = new LineDataSet(hydrationValues, "Hydration");
+
+        hydrationSet.setFillAlpha(110);
+        hydrationSet.setColor(Color.BLUE);
+        hydrationSet.setLineWidth(3f);
+        hydrationSet.setValueTextSize(16);
+
+        // Sleep
+        ArrayList<Entry> sleepValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            sleepValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet sleepSet = new LineDataSet(sleepValues, "Sleep");
+
+        sleepSet.setFillAlpha(110);
+        sleepSet.setColor(Color.GREEN);
+        sleepSet.setLineWidth(3f);
+        sleepSet.setValueTextSize(16);
+
+        // Calories
+        ArrayList<Entry> caloriesValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            caloriesValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet caloriesSet = new LineDataSet(caloriesValues, "Calories");
+
+        caloriesSet.setFillAlpha(110);
+        caloriesSet.setColor(Color.MAGENTA);
+        caloriesSet.setLineWidth(3f);
+        caloriesSet.setValueTextSize(16);
+
+        // Exercise
+        ArrayList<Entry> exerciseValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            exerciseValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet exerciseSet = new LineDataSet(exerciseValues, "Exercise");
+
+        exerciseSet.setFillAlpha(110);
+        exerciseSet.setColor(Color.CYAN);
+        exerciseSet.setLineWidth(3f);
+        exerciseSet.setValueTextSize(16);
+
+        // Blood pressure
+        ArrayList<Entry> bloodPressureValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            bloodPressureValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet bloodPressureSet = new LineDataSet(bloodPressureValues, "Blood Pressure");
+
+        bloodPressureSet.setFillAlpha(110);
+        bloodPressureSet.setColor(Color.YELLOW);
+        bloodPressureSet.setLineWidth(3f);
+        bloodPressureSet.setValueTextSize(16);
+
+        // Weight
+        ArrayList<Entry> weightValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            weightValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet weightSet = new LineDataSet(weightValues, "Weight");
+
+        weightSet.setFillAlpha(110);
+        weightSet.setColor(Color.BLACK);
+        weightSet.setLineWidth(3f);
+        weightSet.setValueTextSize(16);
+
+        // Steps
+        ArrayList<Entry> stepsValues = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i += 10) {
+            stepsValues.add(new Entry(i / 10, (float)(100 * Math.random())));
+        }
+
+        LineDataSet stepsSet = new LineDataSet(stepsValues, "Steps");
+
+        stepsSet.setFillAlpha(110);
+        stepsSet.setColor(Color.DKGRAY);
+        stepsSet.setLineWidth(3f);
+        stepsSet.setValueTextSize(16);
+
+        // Set data
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
-
+        dataSets.add(heartRateSet);
+        dataSets.add(hydrationSet);
+        dataSets.add(sleepSet);
+        dataSets.add(caloriesSet);
+        dataSets.add(exerciseSet);
+        dataSets.add(bloodPressureSet);
+        dataSets.add(weightSet);
+        dataSets.add(stepsSet);
         LineData data = new LineData(dataSets);
-
         chart.setData(data);
 
         String[] values = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"};
