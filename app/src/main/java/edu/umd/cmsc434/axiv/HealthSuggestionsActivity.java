4141d03e39e50a675aa6cb24c583c3b7360b0895
Michael Williams <mjwilli8@umd.edu>
2019-04-22 02:32:02 -0400

    added meaningful health suggestions with more info

--- app/src/main/java/edu/umd/cmsc434/axiv/HealthSuggestionsActivity.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/HealthSuggestionsActivity.java
@@ -24,9 +24,40 @@ public class HealthSuggestionsActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.health_suggestions_list);
 
         final List<String> healthSuggestions = new ArrayList<>();
+        // Sleep
+        healthSuggestions.add("Avoid using your phone right before you go to bed.");
+        // Diet/calories
+        healthSuggestions.add("Drink almond milk rather than cow's milk.");
+        // Exercise
         healthSuggestions.add("Walk 5 miles every week.");
-        healthSuggestions.add("Read for 30 min before going to bed.");
-        healthSuggestions.add("Eat a salad for lunch.");
+        // Steps
+        healthSuggestions.add("Hit 10,000 steps every day.");
+        // Blood pressure
+        healthSuggestions.add("Reduce sodium in your diet.");
+        // Heart rate
+        healthSuggestions.add("Frequently measure your resting heart rate.");
+        // Weight
+        healthSuggestions.add("Eat your food slowly.");
+        // Hydration
+        healthSuggestions.add("Weigh yourself before and after exercise.");
+
+        final List<String> moreInformation = new ArrayList<>();
+        // Sleep:
+        moreInformation.add("Using your phone will stimulate your brain, making it more difficult to fall asleep and stay asleep.");
+        // Diet/calories:
+        moreInformation.add("Almond milk is significantly less calories, low in sugar, and high in vitamins.");
+        // Exercise:
+        moreInformation.add("Walking is a simple exercise that can help improve cholesterol levels, strengthen bones, and keep blood pressure in check.");
+        // Steps:
+        moreInformation.add("Tracking your steps is an easy way to gauge your overall activity level.");
+        // Blood pressure:
+        moreInformation.add("A small reduction of sodium intake can greatly improve your heart and reduce blood pressure significantly.");
+        // Heart rate:
+        moreInformation.add("Your resting heart rate can helpful for gauging your current heart health.");
+        // Weight:
+        moreInformation.add("Fast eaters tend to gain more weight over time. When you eat slower then you will feel more full after eating less.");
+        // Hydration:
+        moreInformation.add("Sweating can contribute to dehydration, especially during the summer. Keep track of your sweat losses to ensure you are staying hydrated.");
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
@@ -42,7 +73,7 @@ public class HealthSuggestionsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 builder.setTitle(healthSuggestions.get(position));
-                builder.setMessage(R.string.more_information);
+                builder.setMessage(moreInformation.get(position));
                 builder.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // Dialog will be dismissed when clicked
