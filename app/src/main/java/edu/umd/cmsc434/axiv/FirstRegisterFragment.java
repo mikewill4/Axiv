6eade84d05f92073145edd5d64fa06997d6e0e88
Fritz Wiltman <fgwilt1@gmail.com>
2019-04-22 04:04:29 -0400

    Added registration/log in page to beginning, log in has most functionality, registration is limited at the moment.

--- app/src/main/java/edu/umd/cmsc434/axiv/FirstRegisterFragment.java
+++ app/src/main/java/edu/umd/cmsc434/axiv/FirstRegisterFragment.java
@@ -1,13 +1,13 @@
 package edu.umd.cmsc434.axiv;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -17,6 +17,14 @@ import android.widget.TextView;
 public class FirstRegisterFragment extends Fragment {
 
     private LoginFragment loginFragment;
+    private AccountDetailsFragment accountDetailsFragment;
+    private SpecificationsFragment specificationsFragment;
+    private BaselineQuestionsFragment baselineQuestionsFragment;
+    private Button AccountDetailsButton;
+    private Button SpecificationsButton;
+    private Button BaselineQuestionsButton;
+    private Button CompleteRegistration;
+
 
     public FirstRegisterFragment() {
         // Required empty public constructor
@@ -32,6 +40,43 @@ public class FirstRegisterFragment extends Fragment {
         haveAccount.setText(Html.fromHtml("<font color=#000000>Already have an account?</font> <font color=#fda500> Sign in.</font>"));
 
         loginFragment = new LoginFragment();
+        accountDetailsFragment = new AccountDetailsFragment();
+        specificationsFragment = new SpecificationsFragment();
+        baselineQuestionsFragment = new BaselineQuestionsFragment();
+
+        AccountDetailsButton = rootView.findViewById(R.id.btnAccountDetails);
+        SpecificationsButton = rootView.findViewById(R.id.btnSpecs);
+        BaselineQuestionsButton = rootView.findViewById(R.id.btnBaselineQuestions);
+        CompleteRegistration = rootView.findViewById(R.id.btnCompleteRegistration);
+
+        AccountDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((LoginActivity)getActivity()).setFragment(accountDetailsFragment);
+            }
+        });
+
+        SpecificationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((LoginActivity)getActivity()).setFragment(specificationsFragment);
+            }
+        });
+
+        BaselineQuestionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((LoginActivity)getActivity()).setFragment(baselineQuestionsFragment);
+            }
+        });
+
+        CompleteRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         haveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
